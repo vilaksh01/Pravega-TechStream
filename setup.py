6a9b6962e4fd141c8e7941b6cea2b5707e237189
vilaksh01 <44412828+vilaksh01@users.noreply.github.com>
2021-05-16 17:45:06 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PravegaHack',
+    version='1.0.0',
+    packages=[ '' ],
+    url='https://github.com/vilaksh01/Pravega-TechStream',
+    license='MIT',
+    author='sumit',
+    author_email='vilagorithm@gmail.com',
+    description='Pravega Hackathaon- Konnex Updesh'
+)
